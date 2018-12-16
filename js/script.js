950c6e23cbf1d8e56e89eb13106acf23385251dd
Billy Shelton <sheltonbilly@utexas.edu>
2018-12-16 13:42:18 -0600

    Other Job Role
    A text box is created when the user selects other from the job role drop down menu.

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
-//the first text input is put in focus when the page loads
+//the first text input is put in focus when the page
 $(':text:visible:first').focus();
+
+//hide other job title input text field
+$('#other-title').hide();
+
+//if the user selects other from the job role drop down menu, the other job title input text is shown
+$('#title').change(function() {
+	if($('#title').val() === 'other') {
+		$('#other-title').show();		
+	} else {
+		//if the user changes the job role from other, the other job title input text is hidden and cleared
+		$('#other-title').hide();
+		$('#other-title').val('');		
+	}
+});
